8c976fc69a7b36dbcc932705a501978d8fa03298
somekindofwallflower <someone.ana13@gmail.com>
2021-12-23 23:34:59 +0100

    Created the GameWord file which contain a function constructor and two methods update and draw

---
+++ GameWorld.js
@@ -0,0 +1,13 @@
+/* GameWorld is an object which is going to contain all the physical object of the game and in each frame animation will be responsible to update them and to draw them on tha canvas */
+
+function GameWorld() {
+
+}
+
+GameWorld.prototype.update = function() {
+
+}
+
+GameWorld.prototype.draw = function() {
+  
+}
