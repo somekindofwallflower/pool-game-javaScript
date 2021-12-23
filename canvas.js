d13426ef7ce338eb87ada5672bd822c953547725
somekindofwallflower <someone.ana13@gmail.com>
2021-12-23 22:56:46 +0100

    Fix some small issues that came up during the testing of draw and clear methods

--- canvas.js
+++ canvas.js
@@ -1,12 +1,22 @@
 function Canvas2D() {
-  this._canvas = document.getElementById("#screen");
+  this._canvas = document.getElementById("screen");
   this._canvasContext = this._canvas.getContext('2d');
 }
 
 Canvas2D.prototype.clear = function() {
-  this._canvasContext.clearRect(0, 0, this._canvas.clientWidth, this._canvas.height);
+  this._canvasContext.clearRect(0, 0, this._canvas.width, this._canvas.height);
 }
 
 Canvas2D.prototype.drawImage = function(image, position) {
   this._canvasContext.drawImage(image, position.x, position.y)
 }
+
+let Canvas = new Canvas2D();
+
+// Testing.
+let image = new Image();
+image.src = "./assets/sprites/spr_background4.png"
+setTimeout(() => {
+  Canvas.drawImage(image, {x: 0, y: 0})
+  // Canvas.clear()
+}, 1000)
