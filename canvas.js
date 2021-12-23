2ed0be4eef82da0d5e24c07ff74efeb3475de95b
somekindofwallflower <someone.ana13@gmail.com>
2021-12-23 22:28:06 +0100

    Create functionalities of clear and draw for canvas

--- canvas.js
+++ canvas.js
@@ -2,3 +2,11 @@ function Canvas2D() {
   this._canvas = document.getElementById("#screen");
   this._canvasContext = this._canvas.getContext('2d');
 }
+
+Canvas2D.prototype.clear = function() {
+  this._canvasContext.clearRect(0, 0, this._canvas.clientWidth, this._canvas.height);
+}
+
+Canvas2D.prototype.drawImage = function(image, position) {
+  this._canvasContext.drawImage(image, position.x, position.y)
+}
